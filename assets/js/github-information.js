c4330e67878abe0281edf2c3e24035d47bf73dc3
Axel Lewing <axel_lewing@hotmail.com>
2024-01-07 13:50:01 +0000

    changed the fetchgithubInformaiton

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,8 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
-    ${"#gh-user-data"}.html("");
-    ${"#gh-repo-data"}.html("");
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     var username = $("#gh-username").val();
 
     if (!username) {
@@ -53,7 +53,7 @@ function fetchGitHubInformation(event) {
     );
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
         function (firstResponse, secondResponse) {
@@ -67,6 +67,9 @@ function fetchGitHubInformation(event) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`
                 );
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
